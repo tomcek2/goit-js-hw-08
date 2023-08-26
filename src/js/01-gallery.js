04e1f3d5025520310168ba2b77a4aa892c65615c
Tomek Cekałą <126020134+tomcek2@users.noreply.github.com>
2023-08-26 18:14:18 +0200

    number one done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,53 +1,33 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Add imports above this line
 
 const gallery = document.querySelector('.gallery');
 
 const galleryLinks = [];
 for (let i = 0; i < galleryItems.length; i++) {
-  const galleryItem = document.createElement('div');
-  galleryItem.classList.add('gallery__item');
-  galleryLinks.push(galleryItem);
-
   const galleryLink = document.createElement('a');
-  galleryLink.classList.add('gallery__link');
+  galleryLink.classList.add('gallery__item');
   galleryLink.href = `${galleryItems[i].original}`;
-  galleryItem.append(galleryLink);
+  galleryLinks.push(galleryLink);
 
   const galleryImage = document.createElement('img');
   galleryImage.classList.add('gallery__image');
   galleryImage.src = `${galleryItems[i].preview}`;
-  galleryImage.dataset.source = `${galleryItems[i].original}`;
   galleryImage.alt = `${galleryItems[i].description}`;
   galleryLink.append(galleryImage);
 }
-
 gallery.append(...galleryLinks);
 
-const modalWindow = e => {
-  e.preventDefault();
-
-  const modalWindowClose = e => {
-    if (e.code == 'Escape') {
-      imageInstance.close();
-    }
-  };
-
-  const imageInstance = basicLightbox.create(
-    `<img src="${e.target.dataset.source}" width="800" height="600"></img>`,
-    {
-      onShow: instance => {
-        document.addEventListener('keydown', modalWindowClose);
-      },
-      onClose: instance => {
-        document.removeEventListener('keydown', modalWindowClose);
-      },
-    }
-  );
-  imageInstance.show();
-};
-
-gallery.addEventListener('click', modalWindow);
+const galleryLightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+galleryLightbox.on('show.simplelightbox');
+console.log(galleryItems);
+// Change code below this line
 
 console.log(galleryItems);
