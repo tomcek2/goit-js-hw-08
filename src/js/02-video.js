f9562b3131d2cd9f93e0475963a45199b8420f7f
Tomek Cekałą <126020134+tomcek2@users.noreply.github.com>
2023-08-27 11:57:12 +0200

    2 final

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const timeUpdate = function (data) {
+  const currentTime = data;
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(currentTime));
+  console.log(data.seconds);
+};
+
+player.on('timeupdate', throttle(timeUpdate, 1000));
+
+const getTimeUpdate = localStorage.getItem('videoplayer-current-time');
+const parsedTimeUpdate = JSON.parse(getTimeUpdate);
+player
+  .setCurrentTime(parsedTimeUpdate.seconds)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
